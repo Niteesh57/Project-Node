c46c227229779948c7fdb244f5620a5d638e5391
Niteesh.bv <niteesh.bv@gmail.com>
2024-10-14 21:53:01 +0530

    new 5

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const exp = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const AuthRouter = require('./Controllers/signup');
+const fs = require('fs');
+const https = require('http');
 const helmet = require('helmet');
 const app = exp();
 
 require('dotenv').config();
 
-app.use(function s(req,res,next){
-    let dates = Date.now()
+app.use(function s(req, res, next) {
+    let dates = Date.now();
     res.locals.title = "My App";
-    let t1 = Date.now() - dates
-    console.log(t1)
+    let t1 = Date.now() - dates;
+    console.log(t1);
     next();
-})
+});
+
 app.set('view engine', 'ejs');
 app.use(helmet());
 app.use(exp.static('public'));
@@ -21,12 +24,14 @@ app.use(expressLayouts);
 app.set('layout', './Layouts/unauth.layout.ejs');
 
 app.use(AuthRouter);
+
 // Handle 404 errors
 app.use((req, res) => {
     res.status(404).send('Page Not Found');
 });
 
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Replace `public_key.pem` with `certificate.pem`
+https.createServer({
+}, app).listen(3000, () => {
+    console.log('HTTPS Server is running on port 3000');
 });
